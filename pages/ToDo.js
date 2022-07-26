482f8ae73a559fd47e0594c6cc1ed6ede39f6c63
Chandan Bangalore Shankar Denanath <denanathchandan5@gmail.com>
2022-07-25 23:27:28 -0400

    Incomplete and Complete Actions addded

--- pages/ToDo.js
+++ pages/ToDo.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import {
   StyleSheet,
@@ -11,10 +10,10 @@ import {
 } from "react-native";
 import uuid from "react-native-uuid";
 
-// uuid.v4()
 export default function ToDo() {
   const [tickets, setTickets] = useState([]);
   const [newTicket, setNewTicket] = useState(undefined);
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
@@ -28,24 +27,66 @@ export default function ToDo() {
           style={styles.add}
           title="Add"
           onPress={() => {
-            if (newTicket !== undefined)
-              setTickets([...tickets, [uuid.v4(), newTicket]]);
-            else Alert.alert("Enter ToDo description");
+            if (newTicket != undefined && newTicket.length > 0) {
+              setTickets([...tickets, [uuid.v4(), newTicket, false]]);
+            } else Alert.alert("Enter ToDo description");
             setNewTicket(undefined);
           }}
         />
       </View>
+      <Text style={styles.heading}>Incomplete:</Text>
+
       <FlatList
-        data={tickets}
+        style={styles.incompleteList}
+        data={
+          tickets.length > 0
+            ? tickets.filter((ticket) => ticket[2] == false)
+            : tickets
+        }
         renderItem={({ item }) => (
           <View style={styles.ticketContainer}>
             <Text style={styles.item}>{item[1]}</Text>
-            {/* <Button
-              title="Complete"
+            <Button
+              title="Incomplete"
+              onPress={() => {
+                setTickets(
+                  tickets.map((val) => {
+                    if (val[0] == item[0]) {
+                      val[2] = true;
+                    }
+                    return val;
+                  })
+                );
+              }}
+            />
+            <Button
+              title="Remove"
               onPress={() => {
                 setTickets(tickets.filter((ticket) => ticket[0] !== item[0]));
               }}
-            /> */}
+            />
+          </View>
+        )}
+      />
+
+      <Text style={styles.heading}>Completed:</Text>
+      <FlatList
+        style={styles.incompleteList}
+        data={tickets.filter((ticket) => ticket[2] == true)}
+        renderItem={({ item }) => (
+          <View style={styles.ticketContainer}>
+            <Text style={styles.item}>{item[1]}</Text>
+            <Button
+              title="Complete"
+              onPress={() => {
+                setTickets(
+                  [...tickets].map((val) => {
+                    if (val[0] == item[0]) val[2] = false;
+                    return val;
+                  })
+                );
+              }}
+            />
             <Button
               title="Remove"
               onPress={() => {
@@ -66,10 +107,12 @@ const styles = StyleSheet.create({
     width: "90%",
   },
   item: {
+    flex: 1,
     padding: 10,
     marginTop: 5,
     fontSize: 18,
     height: 44,
+    width: "30%",
   },
   input: {
     padding: 10,
@@ -88,5 +131,10 @@ const styles = StyleSheet.create({
   ticketContainer: {
     display: "flex",
     flexDirection: "row",
+  },
+  heading: {
+    paddingTop: 10,
+    fontSize: 20,
+    textAlign: "center",
   },
 });
